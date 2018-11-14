35865a2e2a1459efec80461c9da03cfb1f799e09
Williams <cmwilliams@csgralmac13.attlocal.net>
2018-11-13 21:47:41 -0500

    intial commit

---
+++ GirlsWhoCodeDemo/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace GirlsWhoCodeDemo
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            var random = new Random();
+            var randX = random.Next(-300, 300);
+            var randY = random.Next(-300, 300);
+            box.TranslateTo(randX, randY, 250, Easing.CubicIn);
+        }
+    }
+}
---
+++ READ.md
@@ -0,0 +1,1 @@
+# HawksWhoCode.Xamarin
